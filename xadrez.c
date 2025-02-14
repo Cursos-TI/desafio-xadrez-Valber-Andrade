1c48930835839f0d0fd631abae668b6863a31bc3
Valber-Andrade <168591912+Valber-Andrade@users.noreply.github.com>
2025-02-14 01:55:51 +0000

    Feito manutenção e correções no codigo

--- xadrez.c
+++ xadrez.c
@@ -1,19 +1,44 @@
 #include <stdio.h> //Incluindo a biblioteca
 
 int main() {
-    int bispo, torre, rainha = 0; //Adicionando as variaveis de incremento
+    //Adicionando as variaveis de incremento
+    int bispo = 0; 
+    int torre = 0;
+    int rainha = 0;
 
-
-    printf("O Bispo está se movendo na direção:\n"); //Printf para ilustrar a movimentação do Bispo
+    printf("O Bispo está se movendo na direção: \n"); //Printf para ilustrar a movimentação do Bispo
 
     while (bispo < 5)//enquanto o bispo for menor que 5
     {
         printf("Cima, "); printf("Direita.\n");//imprime as direções que ele vai andar
         bispo++; //bispo mais mais, para incremento nas casas
+    
+    }
+
+    
+    
+    printf("A Torre está se movendo na direção: \n"); //Printf para ilustrar a movimentação da Torre
+
+    do //Ira fazer o printf, enquanto o valor de torre for menor que 5
+    {
+        printf("Direita.\n");//imprime as direções que ela vai andar
+        torre++; //torre mais mais, para incremento nas casas
+
+    } while (torre < 5);
+
+
+    printf("A Rainha está se movendo na direção: \n"); //Printf para ilustrar a movimentação da Rainha
+
+    for (rainha = 0; rainha < 8; rainha++)//rainha igual a 0, enquanto a rainha andar menos que 8 casas, rainha++
+    {
+        printf("Esquerda.\n");//imprime as direções que ela vai andar
     }
     
-    do
     
+
+
+    
+
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
